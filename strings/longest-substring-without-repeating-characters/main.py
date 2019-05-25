caebb5b5e189e3f5e975c3c58066310186067023
Zhixuan <1905492410@qq.com>
2019-05-25 10:38:26 +0800

    merge question about array in to director "array"

--- strings/longest-substring-without-repeating-characters/main.py
+++ strings/longest-substring-without-repeating-characters/main.py
@@ -1,21 +1,13 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) == 0:
-            return 0
-
-        result = {}
-        left = 0
+        indexes_dict = {}
         max_length = 0
+        left = -1
 
         for i, c in enumerate(s):
-            if c in result and result[c] >= left:
-                left = result[c] + 1
-            else:
-                max_length = max(i - left + 1, max_length)
-            result[c] = i
+            if c in indexes_dict and indexes_dict[c] >= left:
+                left = indexes_dict[c] + 1
+            indexes_dict[c] = i
+            max_length = max(i - left + 1, max_length)
 
         return max_length
-
-if __name__ == '__main__':
-    s = Solution()
-    s.lengthOfLongestSubstring("tmmzuxt")
